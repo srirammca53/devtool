2788278442d493a282ab7038706458a6dff56674
venkatnr <oneguyvenkat007@gmail.com>
2012-09-18 15:27:01 +0530

    modification on iteration flow

--- app/controllers/iterations_controller.rb
+++ app/controllers/iterations_controller.rb
@@ -1,18 +1,24 @@
 class IterationsController < ApplicationController
-before_filter :test, :except => [:index, :new, :edit, :create]
+#before_filter :test, :except => [:index, :new, :edit, :show]
 
 def test
-		@project = Project.find(params[:project_id])
-		
-		@iteration = @project.iteration.find(params[:id])
-		
-		@endtime =  @iteration.end_date - Time.now.to_date
-		if @endtime.to_i >= -1
-			@iteration.update_attributes(:status => "Closed")
-			project_iterations_path(@project.id)
-		 else
-			project_iteration_path(@project.id, @iteration.id)
-		 end
+		@project = Project.find(params[:project_id])		
+		@new_params = params[:iteration]
+		raise @new_params.each_value { |key| puts "#{key}" }.inspect
+			flag = 0
+			@project.iteration.each do |it|
+					@endd = it.end_date
+					if @enddate < @endd
+						flag += 1
+					 else
+						flag = 0
+					 end
+			end
+			if flag == 0
+					       redirect_to  "create"
+				else
+						raise "you selecting date is already there".inspect
+			end
 end
 
 def index
@@ -33,10 +39,21 @@ end
 
 def create
   @project = Project.find(params[:project_id])
-     @iteration = @project.iteration.new(params[:iteration])
-       @iteration.save
-
-  render :action => "show"
+     @iteration = @project.iteration.new(params[:iteration]) 
+	raise @iteration.inspect
+	if @iteration.status == "Open"
+			@all_iterations = Iteration.find(:all, :select => "status" ,:conditions => {:status => "Open", :project_id => @project.id}).map(&:status).count
+			if 	@all_iterations < 1
+				@iteration.save
+				redirect_to project_iteration_path(@project.id, @iteration.id )
+		   else
+				flash[:error] ="one iteration is already opened. please select planned"
+				redirect_to new_project_iteration_path(@project.id)
+			end
+	else
+			@iteration.save
+			redirect_to project_iteration_path(@project.id, @iteration.id )
+	end
 end
 
 def update
